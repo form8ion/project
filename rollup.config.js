995c5bfc2fb427a2078228be1444dde3b0d7a5e3
Matt Travi <programmer@travi.org>
2018-04-21 23:21:50 -0500

    build(rollup): configured bundle creation

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+/* eslint import/no-extraneous-dependencies: ['error', {'devDependencies': true}] */
+
+export default {
+  input: 'src/index.js',
+  external: [
+    'mz/fs',
+    'inquirer',
+    'shelljs',
+    'path',
+    'chalk',
+    '@travi/javascript-scaffolder',
+    'mustache',
+    'nodegit',
+    'spdx-license-list/full',
+    'spdx-license-list/simple',
+    'util',
+    'write-yaml'
+  ],
+  output: [
+    {file: 'lib/index.cjs.js', format: 'cjs', sourcemap: true},
+    {file: 'lib/index.es.js', format: 'es', sourcemap: true}
+  ]
+};
