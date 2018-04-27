97b850398c9c4ebe7bf67f93a9f186db2987ada2
Matt Travi <programmer@travi.org>
2018-04-26 21:55:41 -0500

    test(git): defined the integration test when not a git repo

--- test/integration/features/git.feature
+++ test/integration/features/git.feature
@@ -1,6 +1,9 @@
 Feature: Git Repository
 
   Scenario: not a git repo
+    Given the project should not be versioned in git
+    When the project is scaffolded
+    Then the base git files should not be present
 
   Scenario: git repo
     Given the project should be versioned in git
--- test/integration/features/step_definitions/vcs/git-steps.js
+++ test/integration/features/step_definitions/vcs/git-steps.js
@@ -1,16 +1,32 @@
-import {readFile} from 'mz/fs';
+import {exists, readFile} from 'mz/fs';
 import {Given, Then} from 'cucumber';
 import bddStdIn from 'bdd-stdin';
 import {assert} from 'chai';
 
 Given('the project should be versioned in git', async function () {
+  const defaultAnswerAboutInitializingGitRepo = '\n';
+
   bddStdIn(
     'project-name',
     'some project description',
     bddStdIn.keys.down, '\n', '\n',
     'y', '\n',
     'y', '\n',
-    '\n', '\n',
+    defaultAnswerAboutInitializingGitRepo, '\n',
+    bddStdIn.keys.down, '\n', '\n'
+  );
+});
+
+Given('the project should not be versioned in git', async function () {
+  const doNotInitGitRepo = 'n';
+
+  bddStdIn(
+    'project-name',
+    'some project description',
+    bddStdIn.keys.down, '\n', '\n',
+    'y', '\n',
+    'y', '\n',
+    doNotInitGitRepo, '\n',
     bddStdIn.keys.down, '\n', '\n'
   );
 });
@@ -19,4 +35,11 @@ Then('the base git files should be present', async function () {
   const gitAttributes = await readFile(`${process.cwd()}/.gitattributes`);
 
   assert.equal(gitAttributes, '* text=auto');
+  // assert.isTrue(gitDirectoryStats.isDirectory());
+});
+
+Then('the base git files should not be present', async function () {
+  assert.isFalse(await exists(`${process.cwd()}/.git`));
+  assert.isFalse(await exists(`${process.cwd()}/.gitattributes`));
+  assert.isFalse(await exists(`${process.cwd()}/.gitignore`));
 });
