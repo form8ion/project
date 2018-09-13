01300db644df35983e5f0d57e39f77691a5a5cd9
Matt Travi <programmer@travi.org>
2018-09-13 00:30:23 -0500

    feat(vcs-hosts): validated the vcsHost options

--- src/options-validator.js
+++ src/options-validator.js
@@ -4,10 +4,11 @@ import hoek from 'hoek';
 export function validate(options) {
   const validated = joi.validate(options, joi.object({
     languages: joi.object().pattern(/^/, joi.func().arity(1)),
-    overrides: joi.object({
-      copyrightHolder: joi.string(),
-      githubAccount: joi.string()
-    })
+    overrides: joi.object({copyrightHolder: joi.string()}),
+    vcsHosts: joi.object().pattern(/^/, joi.object({
+      scaffolder: joi.func().arity(1).required(),
+      prompt: joi.func().required()
+    }))
   }));
 
   hoek.assert(!validated.error, validated.error);
