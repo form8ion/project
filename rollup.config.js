6f4df8207df190ce80bd6648940e47f584df8ed4
Matt Travi <programmer@travi.org>
2020-02-09 23:15:03 -0600

    build(rollup): enabled automatic package exclusion

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,12 @@
 /* eslint import/no-extraneous-dependencies: ['error', {'devDependencies': true}] */
+import autoExternal from 'rollup-plugin-auto-external';
 
 export default {
   input: 'src/index.js',
+  plugins: [autoExternal()],
   external: [
-    'mz/fs',
-    'inquirer',
-    'shelljs',
-    'path',
-    'chalk',
-    '@travi/javascript-scaffolder',
-    'mustache',
-    'nodegit',
     'spdx-license-list/full',
-    'spdx-license-list/simple',
-    'util',
-    'write-yaml',
-    'git-config',
-    'joi',
-    'hoek'
+    'spdx-license-list/simple'
   ],
   output: [
     {file: 'lib/index.cjs.js', format: 'cjs', sourcemap: true},
