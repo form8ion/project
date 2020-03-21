2dc1bd1c997d35d3ddec1a31e272a52960bc8b9b
Matt Travi <programmer@travi.org>
2020-03-20 22:02:32 -0500

    feat(badges): allowed badge creation without a link

--- src/dependency-updater-scaffolder-test.js
+++ src/dependency-updater-scaffolder-test.js
@@ -1,21 +1,39 @@
 import any from '@travi/any';
 import {assert} from 'chai';
 import sinon from 'sinon';
+import * as prompts from './prompts/questions';
 import scaffoldUpdater from './dependency-updater-scaffolder';
+import {questionNames} from './prompts/question-names';
 
 suite('dependency-updater scaffolder', () => {
+  let sandbox;
+
+  setup(() => {
+    sandbox = sinon.createSandbox();
+
+    sandbox.stub(prompts, 'promptForDependencyUpdaterChoice');
+  });
+
+  teardown(() => sandbox.restore());
+
   test('that the chosen scaffolder is executed with the appropriate options', async () => {
+    const decisions = any.simpleObject();
     const options = any.simpleObject();
     const chosenUpdater = any.word();
     const chosenUpdaterScaffolder = sinon.stub();
     const scaffolders = {...any.simpleObject(), [chosenUpdater]: chosenUpdaterScaffolder};
     const scaffolderResult = any.simpleObject();
+    prompts.promptForDependencyUpdaterChoice
+      .withArgs(scaffolders, decisions)
+      .resolves({[questionNames.DEPENDENCY_UPDATER]: chosenUpdater});
     chosenUpdaterScaffolder.withArgs(options).resolves(scaffolderResult);
 
-    assert.equal(await scaffoldUpdater(scaffolders, chosenUpdater, options), scaffolderResult);
+    assert.equal(await scaffoldUpdater(scaffolders, decisions, options), scaffolderResult);
   });
 
-  test('that that choosing an updater without a defined scaffolder does not result in an error', () => {
-    assert.isUndefined(scaffoldUpdater(any.simpleObject(), any.word(), any.simpleObject()));
+  test('that that choosing an updater without a defined scaffolder does not result in an error', async () => {
+    prompts.promptForDependencyUpdaterChoice.resolves({[questionNames.DEPENDENCY_UPDATER]: any.word()});
+
+    assert.isUndefined(await scaffoldUpdater(any.simpleObject(), any.simpleObject(), any.simpleObject()));
   });
 });
--- src/dependency-updater-scaffolder.js
+++ src/dependency-updater-scaffolder.js
@@ -1,4 +1,11 @@
-export default function (scaffolders, chosenUpdater, options) {
+import {promptForDependencyUpdaterChoice} from './prompts/questions';
+import {questionNames} from './prompts/question-names';
+
+export default async function (scaffolders, decisions, options) {
+  const {[questionNames.DEPENDENCY_UPDATER]: chosenUpdater} = await promptForDependencyUpdaterChoice(
+    scaffolders,
+    decisions
+  );
   const scaffolder = scaffolders[chosenUpdater];
 
   if (scaffolder) return scaffolder(options);
