cc20f4b13875d569b8c3ba63e25d2992864d405a
Matt Travi <programmer@travi.org>
2021-01-24 01:02:56 -0600

    refactor(readme-badges): leveraged the lifter from the readme package
    rather than an embedded implementation

--- src/readme-test.js
+++ src/readme-test.js
@@ -1,9 +1,8 @@
-import fs from 'fs';
-import badgeInjectorPlugin from '@form8ion/remark-inject-badges';
+import {promises as fs} from 'fs';
 import any from '@travi/any';
+import * as readme from '@form8ion/readme';
 import sinon from 'sinon';
 import {assert} from 'chai';
-import * as remark from '../thirdparty-wrappers/remark';
 import scaffoldReadme from './readme';
 
 const badgeFactory = () => ({img: any.url(), link: any.url(), text: any.sentence()});
@@ -12,70 +11,33 @@ const statusBadges = any.objectWithKeys(any.listOf(any.word), {factory: badgeFac
 const contributionBadges = any.objectWithKeys(any.listOf(any.word), {factory: badgeFactory});
 
 suite('scaffold readme', () => {
-  let sandbox, use, remarkProcess;
+  let sandbox;
   const projectName = any.word();
   const projectRoot = any.word();
   const description = any.word();
-  const remarkResults = any.string();
 
   setup(() => {
     sandbox = sinon.createSandbox();
 
-    sandbox.stub(remark, 'default');
-    sandbox.stub(fs, 'writeFileSync');
-
-    use = sinon.stub();
-    remarkProcess = sinon.stub();
-
-    remark.default.returns({use});
+    sandbox.stub(readme, 'lift');
+    sandbox.stub(fs, 'writeFile');
   });
 
   teardown(() => sandbox.restore());
 
   test('that the README has a top-level heading of the project name and includes the description', async () => {
     const badges = {consumer: {}, status: {}, contribution: {}};
-    use.withArgs(badgeInjectorPlugin, badges).returns({process: remarkProcess});
-    remarkProcess
-      .withArgs(sinon.match(`# ${projectName}
-
-${description}`))
-      .yields(null, remarkResults);
 
     await scaffoldReadme({projectName, projectRoot, description, badges});
 
-    assert.calledWith(fs.writeFileSync, `${projectRoot}/README.md`, remarkResults);
-  });
+    assert.calledWith(
+      fs.writeFile,
+      `${projectRoot}/README.md`,
+      sinon.match(`# ${projectName}
 
-  test('that the promise is rejected if there is a processing failure', () => {
-    const error = new Error('from test');
-    use.returns({process: remarkProcess});
-    remarkProcess.yields(error);
-
-    return assert.isRejected(scaffoldReadme({projectName, projectRoot, description}), error)
-      .then(() => assert.notCalled(fs.writeFileSync));
-  });
-
-  suite('badges', () => {
-    test('that badges are separated into consumer, status, and contribution zones', async () => {
-      const badges = {consumer: consumerBadges, status: statusBadges, contribution: contributionBadges};
-      use.withArgs(badgeInjectorPlugin, badges).returns({process: remarkProcess});
-      remarkProcess
-        .withArgs(sinon.match(`
-<!--status-badges start -->
-<!--status-badges end -->
-
-<!--consumer-badges start -->
-<!--consumer-badges end -->
-
-<!--contribution-badges start -->
-<!--contribution-badges end -->
-`))
-        .yields(null, remarkResults);
-
-      await scaffoldReadme({projectName, projectRoot, description, badges});
-
-      assert.calledWith(fs.writeFileSync, `${projectRoot}/README.md`, remarkResults);
-    });
+${description}`)
+    );
+    assert.calledWith(readme.lift, {projectRoot, results: {badges}});
   });
 
   suite('documentation', () => {
@@ -84,9 +46,17 @@ ${description}`))
     test('that usage docs are shown after the contributing badges', async () => {
       const usageDocs = markdownWithBackticksAndForwardSlashes;
       const badges = {consumer: consumerBadges, status: statusBadges, contribution: contributionBadges};
-      use.withArgs(badgeInjectorPlugin, badges).returns({process: remarkProcess});
-      remarkProcess
-        .withArgs(sinon.match(`
+
+      await scaffoldReadme({
+        projectRoot,
+        badges,
+        documentation: {usage: usageDocs}
+      });
+
+      assert.calledWith(
+        fs.writeFile,
+        `${projectRoot}/README.md`,
+        sinon.match(`
 <!--status-badges start -->
 <!--status-badges end -->
 
@@ -99,24 +69,24 @@ ${usageDocs}
 
 <!--contribution-badges start -->
 <!--contribution-badges end -->
-`))
-        .yields(null, remarkResults);
-
-      await scaffoldReadme({
-        projectRoot,
-        badges,
-        documentation: {usage: usageDocs}
-      });
-
-      assert.calledWith(fs.writeFileSync, `${projectRoot}/README.md`, remarkResults);
+`)
+      );
     });
 
     test('that consumer badges are lifted to top level when toc is present and no usage content', async () => {
       const toc = `${any.word()} \`${any.word()}\` ${any.word()}`;
       const badges = {consumer: consumerBadges, status: statusBadges, contribution: contributionBadges};
-      use.withArgs(badgeInjectorPlugin, badges).returns({process: remarkProcess});
-      remarkProcess
-        .withArgs(sinon.match(`
+
+      await scaffoldReadme({
+        projectRoot,
+        badges,
+        documentation: {toc}
+      });
+
+      assert.calledWith(
+        fs.writeFile,
+        `${projectRoot}/README.md`,
+        sinon.match(`
 <!--status-badges start -->
 <!--status-badges end -->
 
@@ -129,24 +99,24 @@ ${toc}
 
 <!--contribution-badges start -->
 <!--contribution-badges end -->
-`))
-        .yields(null, remarkResults);
-
-      await scaffoldReadme({
-        projectRoot,
-        badges,
-        documentation: {toc}
-      });
-
-      assert.calledWith(fs.writeFileSync, `${projectRoot}/README.md`, remarkResults);
+`)
+      );
     });
 
     test('that contribution docs are shown after the contributing badges', async () => {
       const contributingDocs = markdownWithBackticksAndForwardSlashes;
       const badges = {consumer: consumerBadges, status: statusBadges, contribution: contributionBadges};
-      use.withArgs(badgeInjectorPlugin, badges).returns({process: remarkProcess});
-      remarkProcess
-        .withArgs(sinon.match(`
+
+      await scaffoldReadme({
+        projectRoot,
+        badges,
+        documentation: {contributing: contributingDocs}
+      });
+
+      assert.calledWith(
+        fs.writeFile,
+        `${projectRoot}/README.md`,
+        sinon.match(`
 <!--status-badges start -->
 <!--status-badges end -->
 
@@ -159,16 +129,8 @@ ${toc}
 <!--contribution-badges end -->
 
 ${contributingDocs}
-`))
-        .yields(null, remarkResults);
-
-      await scaffoldReadme({
-        projectRoot,
-        badges,
-        documentation: {contributing: contributingDocs}
-      });
-
-      assert.calledWith(fs.writeFileSync, `${projectRoot}/README.md`, remarkResults);
+`)
+      );
     });
   });
 });
--- src/readme.js
+++ src/readme.js
@@ -1,27 +1,18 @@
-import fs, {promises} from 'fs';
+import {promises as fs} from 'fs';
 import path from 'path';
 import mustache from 'mustache';
 import {info} from '@travi/cli-messages';
-import badgeInjectorPlugin from '@form8ion/remark-inject-badges';
-import remark from '../thirdparty-wrappers/remark';
+import {lift as liftReadme} from '@form8ion/readme';
 
 export default async function ({projectName, projectRoot, description, badges, documentation}) {
   info('Generating README');
 
   const initialRender = mustache.render(
-    await promises.readFile(path.resolve(__dirname, '..', 'templates/README.mustache'), 'utf8'),
+    await fs.readFile(path.resolve(__dirname, '..', 'templates/README.mustache'), 'utf8'),
     {projectName, description, documentation}
   );
 
-  return new Promise((resolve, reject) => {
-    remark()
-      .use(badgeInjectorPlugin, badges)
-      .process(initialRender, (err, file) => {
-        if (err) reject(err);
-        else {
-          fs.writeFileSync(`${projectRoot}/README.md`, `${file}`);
-          resolve();
-        }
-      });
-  });
+  await fs.writeFile(`${projectRoot}/README.md`, initialRender);
+
+  return liftReadme({projectRoot, results: {badges}});
 }
