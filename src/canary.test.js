95ea0e010e03c116c08716f23a29b9defed8bc69
Matt Travi <programmer@travi.org>
2023-04-15 15:33:17 -0500

    test(unit): converted the readme test to vitest

--- src/canary.test.js
+++
@@ -1,7 +0,0 @@
-import {assert, describe, it} from 'vitest';
-
-describe('canary', () => {
-  it('should pass', () => {
-    assert.isTrue(true);
-  });
-});
---
+++ src/readme.test.js
@@ -0,0 +1,28 @@
+import * as readme from '@form8ion/readme';
+
+import {afterEach, describe, expect, it, vi} from 'vitest';
+import any from '@travi/any';
+
+import scaffoldReadme from './readme';
+
+vi.mock('@form8ion/readme');
+
+describe('readme', () => {
+  const projectName = any.word();
+  const projectRoot = any.word();
+  const description = any.word();
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should create and lift the README', async () => {
+    const badges = any.simpleObject();
+    const documentation = any.simpleObject();
+
+    await scaffoldReadme({projectName, projectRoot, description, badges, documentation});
+
+    expect(readme.scaffold).toHaveBeenCalledWith({projectRoot, projectName, description});
+    expect(readme.lift).toHaveBeenCalledWith({projectRoot, results: {badges, documentation}});
+  });
+});
