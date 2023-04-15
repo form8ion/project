0985530c784cb65d232513fda864e7c81a59fe29
Matt Travi <programmer@travi.org>
2023-04-15 15:40:27 -0500

    test(unit): converted lift tests to vitest

---
+++ src/lift.test.js
@@ -0,0 +1,35 @@
+import deepmerge from 'deepmerge';
+import * as core from '@form8ion/core';
+import * as readme from '@form8ion/readme';
+
+import {afterEach, describe, expect, it, vi} from 'vitest';
+import any from '@travi/any';
+import {when} from 'jest-when';
+
+import lift from './lift';
+
+vi.mock('deepmerge');
+vi.mock('@form8ion/core');
+vi.mock('@form8ion/readme');
+
+describe('lift', () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should lift the README based on the provided results', async () => {
+    const projectRoot = any.string();
+    const enhancers = any.simpleObject();
+    const vcs = any.simpleObject();
+    const results = any.simpleObject();
+    const enhancerResults = any.simpleObject();
+    const mergedResults = any.simpleObject();
+    when(core.applyEnhancers)
+      .calledWith({results, enhancers, options: {projectRoot, vcs}})
+      .mockResolvedValue(enhancerResults);
+    when(deepmerge.all).calledWith([results, enhancerResults]).mockReturnValue(mergedResults);
+
+    expect(await lift({projectRoot, results, enhancers, vcs})).toEqual(enhancerResults);
+    expect(readme.lift).toHaveBeenCalledWith({projectRoot, results: mergedResults});
+  });
+});
