dc82fa0ca1be50a3a6f1e42c71b73cfd223f25aa
Matt Travi <programmer@travi.org>
2023-07-01 22:37:42 -0500

    test(unit): converted the language-scaffolder test to vitest
    for #1276

---
+++ src/language-scaffolder.test.js
@@ -0,0 +1,22 @@
+import {describe, expect, it, vi} from 'vitest';
+import any from '@travi/any';
+import {when} from 'jest-when';
+
+import {scaffold} from './language-scaffolder';
+
+describe('language scaffolder', () => {
+  it('should scaffold the chosen language', async () => {
+    const options = any.simpleObject();
+    const chosenLanguage = any.word();
+    const scaffolderResult = any.simpleObject();
+    const chosenLanguageScaffolder = vi.fn();
+    const scaffolders = {...any.simpleObject(), [chosenLanguage]: chosenLanguageScaffolder};
+    when(chosenLanguageScaffolder).calledWith(options).mockResolvedValue(scaffolderResult);
+
+    expect(await scaffold(scaffolders, chosenLanguage, options)).toEqual(scaffolderResult);
+  });
+
+  it('should not result in an error when choosing a language without a defined scaffolder', async () => {
+    await scaffold(any.simpleObject(), any.word(), any.simpleObject());
+  });
+});
