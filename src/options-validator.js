6cbc53244ff938b04a336e961f32b37c52059875
Matt Travi <programmer@travi.org>
2023-07-06 15:59:01 -0500

    refactor(options-validator): leveraged the extracted language plugins schema

--- src/options-validator.js
+++ src/options-validator.js
@@ -1,9 +1,11 @@
 import joi from 'joi';
 import * as hoek from '@hapi/hoek';
 
+import languagePluginsSchema from './language/schema';
+
 export function validate(options) {
   const schema = joi.object({
-    languages: joi.object().pattern(/^/, joi.func().arity(1)),
+    languages: languagePluginsSchema,
     overrides: joi.object({copyrightHolder: joi.string()}),
     vcsHosts: joi.object().pattern(/^/, joi.object({
       scaffolder: joi.func().arity(1).required(),
