61ce67b18b825f31929dd3575840eb6cdd111733
Matt Travi <programmer@travi.org>
2023-07-06 20:15:33 -0500

    refactor(options-validator): leveraged the options validation from the core module

--- src/options-validator.js
+++ src/options-validator.js
@@ -1,21 +1,16 @@
+import {validateOptions} from '@form8ion/core';
 import joi from 'joi';
-import * as hoek from '@hapi/hoek';
 
 import languagePluginsSchema from './language/schema';
 import vcsHostPluginsSchema from './vcs/host/schema';
 import dependencyUpdaterPluginsSchema from './dependency-updater/schema';
 
 export function validate(options) {
-  const schema = joi.object({
+  return validateOptions(joi.object({
     languages: languagePluginsSchema,
     overrides: joi.object({copyrightHolder: joi.string()}),
     vcsHosts: vcsHostPluginsSchema,
     decisions: joi.object(),
     dependencyUpdaters: dependencyUpdaterPluginsSchema
-  });
-  const validated = schema.validate(options);
-
-  hoek.assert(!validated.error, validated.error);
-
-  return validated.value || {};
+  }), options) || {};
 }
