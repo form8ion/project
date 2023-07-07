ba163080ef98a78f5efc110968f6a95584a5f1d3
Matt Travi <programmer@travi.org>
2023-07-06 22:15:52 -0500

    test(unit): converted the options-validator tests to vitest and accounted for extracted schemas
    for #1276

---
+++ src/options-validator.test.js
@@ -0,0 +1,40 @@
+import joi from 'joi';
+import {validateOptions} from '@form8ion/core';
+
+import {describe, expect, it, beforeEach, afterEach, vi} from 'vitest';
+import any from '@travi/any';
+import {when} from 'jest-when';
+
+import languagePluginsSchema from './language/schema';
+import {decisionsSchema, overridesSchema} from './options-schemas';
+import vcsHostPluginsSchema from './vcs/host/schema';
+import dependencyUpdaterPluginsSchema from './dependency-updater/schema';
+import {validate} from './options-validator';
+
+vi.mock('@form8ion/core');
+
+describe('options validator', () => {
+  beforeEach(() => {
+    vi.spyOn(joi, 'object');
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should build the full schema and call the base validator', () => {
+    const options = any.simpleObject();
+    const fullSchema = any.simpleObject();
+    const validatedOptions = any.simpleObject();
+    when(joi.object).calledWith({
+      languages: languagePluginsSchema,
+      overrides: overridesSchema,
+      vcsHosts: vcsHostPluginsSchema,
+      decisions: decisionsSchema,
+      dependencyUpdaters: dependencyUpdaterPluginsSchema
+    }).mockReturnValue(fullSchema);
+    when(validateOptions).calledWith(fullSchema, options).mockReturnValue(validatedOptions);
+
+    expect(validate(options)).toEqual(validatedOptions);
+  });
+});
