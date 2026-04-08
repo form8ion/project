d7533a394b9a985973192bafb22d147130709040
Matt Travi <programmer@travi.org>
2026-04-07 20:36:33 -0500

    feat(editorconfig): scaffold editorconfig when lifting if not already in use

--- src/lift.js
+++ src/lift.js
@@ -2,9 +2,14 @@ import {applyEnhancers} from '@form8ion/core';
 import {lift as liftReadme} from '@form8ion/readme';
 import * as gitPlugin from '@form8ion/git';
 
+import {scaffold as scaffoldEditorconfig, test as editorconfigInUse} from './editorconfig/index.js';
 import * as licensePlugin from './license/index.js';
 
 export default async function lift({projectRoot, results, enhancers, vcs, dependencies}) {
+  if (!await editorconfigInUse({projectRoot})) {
+    await scaffoldEditorconfig({projectRoot});
+  }
+
   const enhancerResults = await applyEnhancers({
     results,
     enhancers: {...enhancers, gitPlugin, licensePlugin},
--- src/lift.test.js
+++ src/lift.test.js
@@ -2,29 +2,28 @@ import * as core from '@form8ion/core';
 import * as readme from '@form8ion/readme';
 import * as gitPlugin from '@form8ion/git';
 
-import {afterEach, describe, expect, it, vi} from 'vitest';
+import {beforeEach, describe, expect, it, vi} from 'vitest';
 import any from '@travi/any';
 import {when} from 'vitest-when';
 
+import {scaffold as scaffoldEditorconfig, test as editorconfigInUse} from './editorconfig/index.js';
 import * as licensePlugin from './license/index.js';
 import lift from './lift.js';
 
 vi.mock('deepmerge');
 vi.mock('@form8ion/core');
 vi.mock('@form8ion/readme');
+vi.mock('./editorconfig/index.js');
 
 describe('lift', () => {
-  afterEach(() => {
-    vi.clearAllMocks();
-  });
+  const projectRoot = any.string();
+  const results = any.simpleObject();
+  const enhancers = any.simpleObject();
+  const vcs = any.simpleObject();
+  const dependencies = any.simpleObject();
+  const enhancerResults = any.simpleObject();
 
-  it('should lift the README based on the provided results', async () => {
-    const projectRoot = any.string();
-    const enhancers = any.simpleObject();
-    const dependencies = any.simpleObject();
-    const vcs = any.simpleObject();
-    const results = any.simpleObject();
-    const enhancerResults = any.simpleObject();
+  beforeEach(() => {
     when(core.applyEnhancers)
       .calledWith({
         results,
@@ -33,8 +32,21 @@ describe('lift', () => {
         dependencies
       })
       .thenResolve(enhancerResults);
+  });
+
+  it('should lift the README based on the provided results', async () => {
+    when(editorconfigInUse).calledWith({projectRoot}).thenResolve(true);
 
     expect(await lift({projectRoot, results, enhancers, vcs, dependencies})).toEqual(enhancerResults);
     expect(readme.lift).toHaveBeenCalledWith({projectRoot, results: enhancerResults});
+    expect(scaffoldEditorconfig).not.toHaveBeenCalled();
+  });
+
+  it('should scaffold editorconfig when it isnt already in use', async () => {
+    when(editorconfigInUse).calledWith({projectRoot}).thenResolve(false);
+
+    await lift({projectRoot, results, enhancers, vcs, dependencies});
+
+    expect(scaffoldEditorconfig).toHaveBeenCalledWith({projectRoot});
   });
 });
